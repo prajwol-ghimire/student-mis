0e9f69ed3ac32d866650683307c5ce2a77ad2c67
prajwolq <prajwolghimire19@gmail.com>
2023-07-08 15:32:02 +0545

    required modified

--- app/routers/otpValidate.js
+++ app/routers/otpValidate.js
@@ -15,18 +15,23 @@ async function compareOTP(hashedotp, otpvalue, res,sid_user) {
 async function otpValidate(req, res) {
     const otpvalue = req.body.otpvalue;
     const sid_user = req.body.sid_user;
-    let qry = "select otp_temp from user_infos where sid = ?";
-    mysql.query(qry, sid_user, (err, recivedresults) => {   
-        if(err) throw err
-        else{  
-            const extractedData = recivedresults.map(row => {
-                const hashedotp = row.otp_temp;
-                compareOTP(hashedotp,otpvalue,res,sid_user);       
-            });
-              
-         
-        }
-    });
+    if (otpvalue == null && sid_user == null){
+        let qry = "select otp_temp from user_infos where sid = ?";
+        mysql.query(qry, sid_user, (err, recivedresults) => {   
+            if(err) throw err
+            else{  
+                const extractedData = recivedresults.map(row => {
+                    const hashedotp = row.otp_temp;
+                    compareOTP(hashedotp,otpvalue,res,sid_user);       
+                });
+                
+            
+            }
+        });
+    }
+    else{
+        res.render("otpverify.hbs", {blankvalue : true,sid : sid_user})
+    }
     // const result = await bcrypt.compare(otpvalue, hashedpassword);
 
 }
