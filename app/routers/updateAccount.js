2daca48cc5dfe47d92587fd0a80b35d4ea183cc7
prajwolq <prajwolghimire19@gmail.com>
2023-07-14 23:09:24 +0545

    Account settings done

--- app/routers/updateAccount.js
+++ app/routers/updateAccount.js
@@ -21,6 +21,7 @@ function updateAccount(req, res){
                     else {
                         res.redirect("/profile-setting")
                     }
+                    
                 });
             }
         });
